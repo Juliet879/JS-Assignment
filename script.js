09374e7cb653834ed84d22ec260314f78f416b75
juliet879 <gisembajuliett@gmail.com>
2021-03-09 11:33:38 +0300

    created the function

---
+++ script.js
@@ -0,0 +1,8 @@
+function fruits(){
+    var fruit_list = ["Mangoes","Pineaple","Guava","Strawberry"]
+    for(var i=0; i<fruit_list.length; i++){
+        console.log(fruit_list[i])
+    }
+}
+fruits();
+
